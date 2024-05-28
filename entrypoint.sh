e8e06285a4a135d4fb41365626f4c3b3d30ae79c
Jan Hansmeyer <j-hansmeyer@gmx.de>
2024-05-28 12:51:14 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,3 +2,6 @@
 
 echo "$DCU_CRONTAB_EXECUTION_EXPRESSION /opt/docker_container_updater/container_update.sh" > /etc/crontabs/root
 crond -f
+postconf -e "relayhost = $(echo $DCU_MAIL_RELAYHOST)"
+postfix stop
+postfix start
