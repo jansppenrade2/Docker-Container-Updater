2e7403d28a82a4678029fc0949fa0c977819758e
Jan Hansmeyer <j-hansmeyer@gmx.de>
2024-06-05 22:05:10 +0000

    Testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,15 +10,19 @@ if { [[ "$1" = "dcu" ]] && [ -z "$2" ]; } || [ -z "$1" ]; then
     echo "Starting crond in foreground..."
     crond -f
 elif [ "$1" = "dcu" ] && [ "$2" = "--self-update" ]; then
+    echo "INFO   Waiting for status update..."
     start_time=$(date +%s)
     while [[ ! -f "/opt/dcu/.main_update_process_completed" ]]; do
         now_time=$(date +%s)
         if (( now_time - start_time >= 3600 )); then
-            echo "ERROR  timeout reached!"
-            echo "INFO   exiting..."
+            echo "ERROR  Timeout reached!"
+            echo "INFO   Exiting..."
             sleep 10
             exit 1
         fi
         sleep 10
     done
+    echo "INFO   Proceeding with self-update process..."
+    echo "INFO       Executing \"/opt/dcu/dcu_main.sh\""
+    /opt/dcu/dcu_main.sh
 fi
