41eab5698ac19834fbe48468176b2c6c8e347d06
Jan Hansmeyer <j-hansmeyer@gmx.de>
2024-06-05 02:31:37 +0000

    Testing

--- container_update.sh
+++ container_update.sh
@@ -4,7 +4,7 @@
 # Automatic Docker Container Updater Script
 #
 # ## Version
-# 2024.06.04-g
+# 2024.06.04-h
 #
 # ## Changelog
 # 2024.06.04-1, janseppenrade2: Issue: Fixed an issue that caused ading non-persitant mounts to docker run command (by previous bugfix in version 2024.06.03-1). Added support for self-update.
@@ -1904,6 +1904,7 @@ Perform-ImageUpdate() {
         [ "$test_mode" == false ] && [ "$image_pulled_successfully" == true  ] && echo "$cmd_docker stop \"$container_name\"" >> "$self_update_script_file"                                 || Write-Log "ERROR" "             => Failed to add instructions to (\"$self_update_script_file\")"
         [ "$test_mode" == false ] && [ "$image_pulled_successfully" == true  ] && echo "$docker_run_cmd" >> "$self_update_script_file"                                                      || Write-Log "ERROR" "             => Failed to add instructions to (\"$self_update_script_file\")"
         [ "$test_mode" == false ] && [ "$image_pulled_successfully" == true  ] && echo "$cmd_docker stop \""$container_name"_SelfUpdateHelper\"" >> "$self_update_script_file"              || Write-Log "ERROR" "             => Failed to add instructions to (\"$self_update_script_file\")"
+        [ "$test_mode" == false ] && [ "$image_pulled_successfully" == true  ] && chmod +x "$self_update_script_file"                                                                       || Write-Log "ERROR" "             => Failed to change permissions of (\"$self_update_script_file\")"
 
         # Run self-update helper container
         [ "$test_mode" == false ] && [ "$image_pulled_successfully" == true  ] && Write-Log "INFO"  "           Bringing up self-update helper container..."
